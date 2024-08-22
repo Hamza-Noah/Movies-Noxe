b442bf5d12526acc11247319a9b2d9c19008a440
Hamza-Noah <hamza.nouh97@gmail.com>
2024-08-23 01:38:02 +0300

    linked the context to the navbar to enable conditional rendering

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../Contexts/AuthContext";
 
 export default function Navbar() {
+  const { userToken, setUserToken } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    setUserToken("");
+    navigate("/login");
+  }
+
   return (
     <>
       <nav className="navbar navbar-expand-lg my-2">
@@ -20,26 +30,30 @@ export default function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active fw-bolder"
-                  aria-current="page"
-                  to="/"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link fw-bolder" to="/people">
-                  People
-                </Link>
-              </li>
+              {userToken && (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active fw-bolder"
+                      aria-current="page"
+                      to="/"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link fw-bolder" to="/people">
+                      People
+                    </Link>
+                  </li>
 
-              <li className="nav-item">
-                <Link className="nav-link fw-bolder" to="/tvshows">
-                  Tv shows
-                </Link>
-              </li>
+                  <li className="nav-item">
+                    <Link className="nav-link fw-bolder" to="/tvshows">
+                      Tv shows
+                    </Link>
+                  </li>
+                </>
+              )}
             </ul>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <form className="d-flex" role="search">
@@ -51,23 +65,35 @@ export default function Navbar() {
                 />
               </form>
 
-              <li className="nav-item">
-                <Link
-                  className="nav-link active fw-bolder"
-                  aria-current="page"
-                  to="/register"
-                >
-                  Register
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link fw-bolder" to="/Login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link fw-bolder">Logout</Link>
-              </li>
+              {!userToken && (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link active fw-bolder"
+                      aria-current="page"
+                      to="/register"
+                    >
+                      Register
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link fw-bolder" to="/Login">
+                      Login
+                    </Link>
+                  </li>
+                </>
+              )}
+              {userToken && (
+                <li className="nav-item">
+                  <button
+                    type="button"
+                    onClick={handleLogout}
+                    className="nav-link fw-bolder"
+                  >
+                    Logout
+                  </button>
+                </li>
+              )}
             </ul>
           </div>
         </div>
