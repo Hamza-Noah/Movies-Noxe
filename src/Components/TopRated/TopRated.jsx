7bc0d42b793dd76a5b8e93826c9fc67d107d0005
Hamza-Noah <hamza.nouh97@gmail.com>
2024-08-28 16:49:52 +0300

    fetched the movies to from top rated endpoint

--- src/Components/TopRated/TopRated.jsx
+++ src/Components/TopRated/TopRated.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import axios from 'axios'
+import {useEffect, useState} from 'react';
+import Movie from '../Movie/Movie';
 
 export default function TopRated() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  async function getMovies() {
+    try {
+      const { data } = await axios.get(
+        "https://api.themoviedb.org/3/movie/top_rated?api_key=f31de6b44ad5fe28935eff41302201c7"
+      );
+
+      setMovies(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  console.log(movies);
+  
   return (
-    <div>TopRated</div>
-  )
+    <>
+     <div className="container mt-5 pt-5">
+        <h1 className="mb-5">
+          <span className="d-block mb-3"> Welcome.</span>
+          Millions of movies, TV shows and people to discover. Explore now.
+        </h1>
+        <div className="row g-3 mt-5">
+          {movies.map((movie, index) => {
+            return <Movie movie={movie} key={index}></Movie>;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
