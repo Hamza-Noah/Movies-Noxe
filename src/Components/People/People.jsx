e2da72891d26db6b0948ccc56e8a1d837ee2cb62
Hamza-Noah <hamza.nouh97@gmail.com>
2024-08-28 16:31:49 +0300

    fetched the people

--- src/Components/People/People.jsx
+++ src/Components/People/People.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Person from "../Person/Person";
 
 export default function People() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  async function getMovies() {
+    const { data } = await axios.get(
+      "https://api.themoviedb.org/3/trending/person/day?api_key=3a8d4bff99757bb1b549c063f2ed3401"
+    );
+
+    setMovies(data.results);
+  }
+  console.log(movies);
+
   return (
-    <div>People</div>
-  )
+    <>
+      <div className="container mt-5 pt-5">
+        <h1 className="mb-5">
+          <span className="d-block mb-3"> Welcome.</span>
+          Millions of movies, TV shows and people to discover. Explore now.
+        </h1>
+        <div className="row g-3 mt-5">
+          {movies.map((movie, index) => {
+            return <Person person={movie} key={index}></Person>;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
