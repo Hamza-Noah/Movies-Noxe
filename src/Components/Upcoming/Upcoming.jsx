11ec42f8fecf7c4d9d0570aaf83975f5ddd256e3
Hamza-Noah <hamza.nouh97@gmail.com>
2024-08-28 16:31:37 +0300

    fetched the movies

--- src/Components/Upcoming/Upcoming.jsx
+++ src/Components/Upcoming/Upcoming.jsx
@@ -1,6 +1,44 @@
+import axios from 'axios'
+import {useEffect, useState} from 'react';
+import Movie from '../Movie/Movie';
+
 
 export default function Upcoming() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  async function getMovies() {
+    try {
+      const { data } = await axios.get(
+        "https://api.themoviedb.org/3/movie/upcoming?api_key=f31de6b44ad5fe28935eff41302201c7"
+      );
+
+      setMovies(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  console.log(movies);
+  
   return (
-    <div>Upcoming</div>
-  )
+    <>
+     <div className="container mt-5 pt-5">
+        <h1 className="mb-5">
+          <span className="d-block mb-3"> Welcome.</span>
+          Millions of movies, TV shows and people to discover. Explore now.
+        </h1>
+        <div className="row g-3 mt-5">
+          {movies.map((movie, index) => {
+            return <Movie movie={movie} key={index}></Movie>;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
