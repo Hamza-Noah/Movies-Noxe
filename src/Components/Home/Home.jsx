8f79e8bf4bd5d982f166cdff7ab2e777402d9d8f
Hamza-Noah <hamza.nouh97@gmail.com>
2024-09-04 13:44:47 +0300

    added the slider to the home comopnent and also choosed the right size for slider

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Movie from "../Movie/Movie";
+import SliderMovies from "../SliderMovies/SliderMovies"
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -14,6 +15,12 @@ export default function Home() {
       "https://api.themoviedb.org/3/movie/now_playing?api_key=f31de6b44ad5fe28935eff41302201c7"
     );
 
+setTimeout(() => {
+  console.log(movies);
+  
+  
+}, 2000);    
+
     setMovies(data.results);
   }
   console.log(movies);
@@ -21,6 +28,7 @@ export default function Home() {
   return (
     <>
       <div className="container mt-5 pt-5">
+        <SliderMovies movies={movies}/>
         <h1 className="mb-5">
           <span className="d-block mb-3"> Welcome.</span>
           Millions of movies, TV shows and people to discover. Explore now.
---
+++ src/Components/SliderMovies/SliderMovies.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import MovieDetails from "../MovieDetails/MovieDetails";
+
+
+export default function SliderMovies({movies}) {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <>
+      <div className="container">
+        <Slider {...settings} >
+        {movies?.map((movie, i) =>{
+          return <div key={i}>
+           <img  className="w-100" src={"https://image.tmdb.org/t/p/original/" + movie.backdrop_path} alt="" />
+          </div>
+        })}
+        </Slider>
+      </div>
+    </>
+  );
+}
